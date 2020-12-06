855c7d7366cdbb980d1504809d4b488cec79bbdc
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2020-12-06 01:02:12 +0100

    cleanup in help

--- c64_kickc.mk
+++ c64_kickc.mk
@@ -112,7 +112,7 @@ define print_help
 	grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(1) | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36mmake %-20s\033[0m%s\n", $$1, $$2}'
 endef
 
-help:
+help: ## full lists of tasks with descriptions
 	@printf "\033[36mHelp: \033[0m\n"
 	@$(foreach file, $(MAKEFILE_LIST), $(call print_help, $(file));)
 
